29e4c5f40d19226766029a18a920119eb4999013
twilight-0po <oth1305@naver.com>
2022-12-23 08:09:00 +0900

    add no

--- read.py
+++ read.py
@@ -5,25 +5,25 @@ print (file_contents)
 f.close()
 
 receiver = input("상품 받는 사람 학번: ")
+if receiver != "0":
+    file = open("student_name.txt", "r", encoding='utf-8')
+    replacement = ""
+    for line in file:
+        line = line.strip()
+        if receiver in line:
+            line = ""
+            replacement = replacement + line
+        else:
+            replacement = replacement + line + "\n"
 
-file = open("student_name.txt", "r", encoding='utf-8')
-replacement = ""
-for line in file:
-    line = line.strip()
-    if receiver in line:
-        line = ""
-        replacement = replacement + line
-    else:
-        replacement = replacement + line + "\n"
+    file.close()
+    # opening the file in write mode
+    fout = open("student_name.txt", "w", encoding='utf-8')
+    fout.write(replacement)
+    fout.close()
 
-file.close()
-# opening the file in write mode
-fout = open("student_name.txt", "w", encoding='utf-8')
-fout.write(replacement)
-fout.close()
-
-f = open('student_name.txt', 'r', encoding='utf-8')
-file_contents = f.read()
-print("=" * 5 + "받는 사람 명단" + "=" * 5)
-print (file_contents)
-f.close()
+    f = open('student_name.txt', 'r', encoding='utf-8')
+    file_contents = f.read()
+    print("=" * 5 + "받는 사람 명단" + "=" * 5)
+    print (file_contents)
+    f.close()
