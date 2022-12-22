aa457133d7202ff9fe45b51e6ea9655dde446a2b
twilight-0po <oth1305@naver.com>
2022-12-22 09:27:58 +0900

    add remove

--- read_student.py
+++ read_student.py
@@ -1,5 +1,29 @@
-f = open('student_name.txt', 'r')
+f = open('student_name.txt', 'r', encoding='utf-8')
 file_contents = f.read()
+print("=" * 5 + "받는 사람 명단" + "=" * 5)
 print (file_contents)
+f.close()
 
+receiver = input("상품 받는 사람 학번: ")
+
+file = open("student_name.txt", "r", encoding='utf-8')
+replacement = ""
+for line in file:
+    line = line.strip()
+    if receiver in line:
+        line = ""
+        replacement = replacement + line
+    else:
+        replacement = replacement + line + "\n"
+
+file.close()
+# opening the file in write mode
+fout = open("student_name.txt", "w", encoding='utf-8')
+fout.write(replacement)
+fout.close()
+
+f = open('student_name.txt', 'r', encoding='utf-8')
+file_contents = f.read()
+print("=" * 5 + "받는 사람 명단" + "=" * 5)
+print (file_contents)
 f.close()
